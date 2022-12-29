bcef4dd7d3e5131bef0bb8841682b1f8c93739dd
Luca Trușcă <t.luca1152@gmail.com>
2022-12-29 17:40:42 +0200

    Draw a 3D cube

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,22 @@
 using namespace glm;
 using namespace std;
 
-GLuint vao, vbo, ebo;
 GLuint shaderProgram;
+GLuint vao, vbo, ebo;
+GLuint viewLocation, projLocation;
+
+// View matrix
+glm::mat4 view;
+float obsX = -Constants::WIDTH / 2.0f, obsY = -Constants::HEIGHT / 2.0f, obsZ = 300.f;
+float refX = 0.0f, refY = 0.0f, refZ = -100.f;
+float vX = 0.0f;
+
+// Projection matrix
+glm::mat4 projection;
+float xMin = 0.0f, xMax = Constants::WIDTH, yMin = 0.0f, yMax = Constants::HEIGHT;
+float dNear = 100.0f, dFar = 500.0f;
+
+glm::vec3 observer, referencePoint, vertical;
 
 GLFWwindow *initializeWindow() {
     if (!glfwInit()) {
@@ -35,20 +49,31 @@ GLFWwindow *initializeWindow() {
 
 void initializeShaders() {
     shaderProgram = ShadersUtils::loadShaders("../src/shaders/shader.vert", "../src/shaders/shader.frag");
-    Constants::MATRIX_LOCATION = glGetUniformLocation(shaderProgram, "matrix");
-}
 
+    // Locations
+    viewLocation = glGetUniformLocation(shaderProgram, "viewShader");
+    projLocation = glGetUniformLocation(shaderProgram, "projectionShader");
+}
 
 void initializeScene() {
     GLfloat vertices[] = {
-            0.5f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, 0.5f, 0.0f
+            //            Coordinates             |             Colors
+            -50.0f, -50.0f, -50.0f, 0.0f, 1.0f, 0.0f,
+            50.0f, -50.0f, -50.0f, 0.0f, 0.9f, 0.0f,
+            50.0f, 50.0f, -50.0f, 0.0f, 0.6f, 0.0f,
+            -50.0f, 50.0f, -50.0f, 0.0f, 0.2f, 0.0f,
+            -50.0f, -50.0f, 50.0f, 1.0f, 0.0f, 0.0f,
+            50.0f, -50.0f, 50.0f, 0.7f, 0.0f, 0.0f,
+            50.0f, 50.0f, 50.0f, 0.5f, 0.0f, 0.0f,
+            -50.0f, 50.0f, 50.0f, 0.1f, 0.0f, 0.0f,
     };
     GLuint indices[] = {
-            0, 1, 3,
-            1, 2, 3
+            1, 0, 2, 2, 0, 3,  //  Bottom
+            2, 3, 6, 6, 3, 7,  // Side
+            7, 3, 4, 4, 3, 0,  // Side
+            4, 0, 5, 5, 0, 1,  // side
+            1, 2, 5, 5, 2, 6,  // Side
+            5, 6, 4, 4, 6, 7, //  Top
     };
 
     glGenVertexArrays(1, &vao);
@@ -62,7 +87,10 @@ void initializeScene() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0); // 0 = position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) 0);
+    glEnableVertexAttribArray(1); // 1 = color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
 
     glEnableVertexAttribArray(0);
 }
@@ -70,8 +98,22 @@ void initializeScene() {
 void render() {
     glUseProgram(shaderProgram);
 
+    // View matrix
+    observer = glm::vec3(obsX, obsY, obsZ);
+    refX = obsX;
+    refY = obsY;
+    referencePoint = glm::vec3(refX, refY, refZ);
+    vertical = glm::vec3(vX, 1.0f, 0.0f);
+    view = glm::lookAt(observer, referencePoint, vertical);
+    glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
+
+    // Projection matrix
+    projection = glm::ortho(xMin, xMax, yMin, yMax, dNear, dFar);
+    glUniformMatrix4fv(projLocation, 1, GL_FALSE, &projection[0][0]);
+
+
     glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
 
@@ -93,7 +135,8 @@ int main() {
 
         glViewport(0, 0, width, height);
         glClearColor(Constants::SKY_COLOR.r, Constants::SKY_COLOR.g, Constants::SKY_COLOR.b, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
 
         render();
 
--- src/shaders/shader.vert
+++ src/shaders/shader.vert
@@ -3,9 +3,12 @@
 layout (location = 0) in vec3 in_Position;
 layout (location = 1) in vec4 in_Color;
 
+uniform mat4 viewShader;
+uniform mat4 projectionShader;
+
 out vec4 ex_Color;
 
 void main() {
-    gl_Position = vec4(in_Position.xyz, 1.0f);
+    gl_Position = projectionShader * viewShader * vec4(in_Position, 1.0);
     ex_Color = in_Color;
 }
