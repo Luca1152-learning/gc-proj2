dc8997337e12f35f737daf27752b5f1b51d16718
Luca Trușcă <t.luca1152@gmail.com>
2022-12-29 15:31:08 +0200

    Change the sky color so it matches the design

--- src/utils/Constants.cpp
+++ src/utils/Constants.cpp
@@ -6,4 +6,4 @@ const int Constants::HEIGHT = 720;
 const mat4 Constants::RESIZE_MATRIX = ortho(0.0f, float(WIDTH), 0.0f, float(HEIGHT));
 GLuint Constants::MATRIX_LOCATION = 0;
 
-const vec3 Constants::SKY_COLOR = Color::fromHex("#9DD5DD");
+const vec3 Constants::SKY_COLOR = Color::fromHex("#B6FBFE");
